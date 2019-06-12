51604b14a30ab0e701ed4d7c699f5d173fbff694
Elia Vicentini <eliavicentini@iMac-Fornacee.homenet.telecomitalia.it>
2019-06-12 09:51:02 +0200

    Update pdfCoverterGui.py - Creata GUI

---
+++ pdfConverterGui.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+
+class Ui_MainWindow(object):
+
+    file = ''
+    azienda = ''
+    filesName = []
+    files = []
+    aziende = []
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(740, 380)
+        MainWindow.setWindowOpacity(1.0)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 20, 471, 31))
+        self.label.setObjectName("label")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(280, 110, 221, 201))
+        self.listWidget.setObjectName("listWidget")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(280, 90, 221, 16))
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 110, 121, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(20, 140, 241, 32))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.listWidget_2 = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget_2.setGeometry(QtCore.QRect(510, 110, 221, 201))
+        self.listWidget_2.setObjectName("listWidget_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(510, 90, 59, 16))
+        self.label_3.setObjectName("label_3")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(140, 110, 121, 32))
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(20, 280, 241, 32))
+        self.pushButton_3.setObjectName("pushButton_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 740, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuHelp = QtWidgets.QMenu(self.menubar)
+        self.menuHelp.setObjectName("menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionGit = QtWidgets.QAction(MainWindow)
+        self.actionGit.setObjectName("actionGit")
+        self.actionEsci = QtWidgets.QAction(MainWindow)
+        self.actionEsci.setObjectName("actionEsci")
+        self.actionCome_funziona = QtWidgets.QAction(MainWindow)
+        self.actionCome_funziona.setObjectName("actionCome_funziona")
+        self.actionAutore_progetto = QtWidgets.QAction(MainWindow)
+        self.actionAutore_progetto.setObjectName("actionAutore_progetto")
+        self.menuFile.addAction(self.actionGit)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionEsci)
+        self.menuHelp.addAction(self.actionCome_funziona)
+        self.menuHelp.addSeparator()
+        self.menuHelp.addAction(self.actionAutore_progetto)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+
+        self.pushButton_2.setEnabled(False)
+
+        self.pushButton.clicked.connect(self.get_file)
+        self.pushButton_2.clicked.connect(self.save)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def get_file(self):
+        name = QFileDialog.getOpenFileName(None, 'Open File', '', '*.pdf')
+        self.files.append(name[0])
+        tempName = str(name[0]).split('/')
+        self.file = tempName[-1]
+        self.filesName.append(self.file)
+        print(self.file)
+        print(self.files)
+        self.pushButton.setEnabled(False)
+        self.pushButton_2.setEnabled(True)
+        self.pushButton_2.update()
+
+    def save(self):
+        self.azienda = self.comboBox.currentText()
+        self.aziende.append(self.azienda)
+
+        self.listWidget.clear()
+        self.listWidget_2.clear()
+        self.azienda = ''
+        self.file = ''
+
+        self.listWidget.addItems(self.filesName)
+        self.listWidget_2.addItems(self.aziende)
+
+        self.pushButton.setEnabled(True)
+        self.pushButton_2.setEnabled(False)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Pdf Converter & Data Analyzer v. 0.0.1"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt; font-weight:600; font-style:italic;\">Pdf Converter &amp; Data Analyzer </span><span style=\" font-size:24pt; font-weight:600; font-style:italic; vertical-align:sub;\">by Vicentini Elia</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "File pronti per la conversione"))
+        self.pushButton.setText(_translate("MainWindow", "Seleziona file"))
+        self.pushButton_2.setText(_translate("MainWindow", "Salva scelta"))
+        self.label_3.setText(_translate("MainWindow", "Azienda"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "Eni"))
+        self.comboBox.setItemText(1, _translate("MainWindow", "Esso"))
+        self.comboBox.setItemText(2, _translate("MainWindow", "Union"))
+        self.pushButton_3.setText(_translate("MainWindow", "Avvia conversione"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuHelp.setTitle(_translate("MainWindow", "Help"))
+        self.actionGit.setText(_translate("MainWindow", "Git"))
+        self.actionEsci.setText(_translate("MainWindow", "Esci"))
+        self.actionCome_funziona.setText(_translate("MainWindow", "Come funziona?"))
+        self.actionAutore_progetto.setText(_translate("MainWindow", "Autore progetto"))
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    MainWindow.update()
+    sys.exit(app.exec_())
--- setupGui.py
+++ setupGui.py
@@ -1,65 +1,222 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'setup.ui'
-#
-# Created by: PyQt5 UI code generator 5.12.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+import getpass
+import sys
+import os
+import wget
+import ssl
+import time
+ssl._create_default_https_context = ssl._create_unverified_context
 
+# -- Link per il download dei files utili --
+LINKPDFTOTEXTWIN = 'https://drive.google.com/uc?export=download&id=19BTAN7mleq7yz2r48GaLPky_q_bvcEuJ'
+LINKPDFTOTEXTMACOS = 'https://drive.google.com/uc?export=download&id=1ZkNeVEojpTKsL9EHwngbCfq4rdEtrx_u'
+
+LINKPDFCONVERTER = 'https://drive.google.com/uc?export=download&id=1ns3jBwMxglkSOLzNU4TzoHD3eI3flLVg'
+LINKDATACOLLECT = 'https://drive.google.com/uc?export=download&id=1uy_ZHjic5lbEK6bQNKTbiXYHXUfHPuWw'
+LINKEXPORTDATA = 'https://drive.google.com/uc?export=download&id=1gh7XkRDZTMo6bwtvXUWUbMquwfgDulvf'
+
+# Variabili globali 
+USERNAME = getpass.getuser()
+LIBRARIES = ['openpyxl', 'PySimpleGUI', 'wget']
+
+PATHDOCUMENTSWIN32 = 'C:\\Users\\' + USERNAME + '\\Documents'
+PATHDOCUMENTSMAC = '/Users/' + USERNAME + '/Documents'
+
+PATHWIN32 = PATHDOCUMENTSWIN32 + '\\PdfConverter-DataAnalyzer'
+PATHMACOS = PATHDOCUMENTSMAC + '/PdfConverter-DataAnalyzerTEST'
+
+PATHSWIN = ['\\lib', '\\pdf','\\txt', '\\output']
+PATHSMACOS = ['/lib', '/pdf','/txt', '/output']
 
 class Ui_MainWindow(object):
+    
     def setupUi(self, MainWindow):
+        """
+            Setup del programma
+        """
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(640, 197)
+        MainWindow.resize(640, 164)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(20, 10, 151, 31))
+        self.label.setGeometry(QtCore.QRect(20, 10, 291, 31))
         self.label.setObjectName("label")
-        self.execution = QtWidgets.QLabel(self.centralwidget)
-        self.execution.setGeometry(QtCore.QRect(20, 50, 341, 51))
-        self.execution.setText("")
-        self.execution.setObjectName("execution")
+        
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setGeometry(QtCore.QRect(20, 110, 591, 23))
-        self.progressBar.setProperty("value", 24)
+        self.progressBar.setGeometry(QtCore.QRect(20, 80, 591, 23))
+        self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
+        self.progressBar.update()
+        
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 50, 181, 16))
+        self.label_2.setObjectName("label_2")
+        
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 22))
         self.menubar.setObjectName("menubar")
+        
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
+        
         MainWindow.setMenuBar(self.menubar)
+        
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
+        
         MainWindow.setStatusBar(self.statusbar)
+        
         self.actionEsci = QtWidgets.QAction(MainWindow)
         self.actionEsci.setObjectName("actionEsci")
+       
         self.menuFile.addAction(self.actionEsci)
+       
         self.menubar.addAction(self.menuFile.menuAction())
 
         self.retranslateUi(MainWindow)
         self.actionEsci.triggered.connect(MainWindow.close)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Setup Program</span></p></body></html>"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Setup"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:18pt;\">Setup PdfConverter &amp; DataAnalyzer</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "Sto installando il programma"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.actionEsci.setText(_translate("MainWindow", "Esci"))
 
+    def main(self):
+        self.checkPyLib()
+        print('Check librerie fatto')
+        self.checkFirstInstallationMac()
+        print('Check directory fatto')
+        self.progressBar.setValue(100)
+        self.progressBar.update
+        if self.progressBar.value() is 100:
+            print("Continua")
+    
+    def downloadUtilities():
+        """
+            Scarica tutte i file necessari per il corretto funzionamento del programma
+        """
+        
+        if sys.platform.__contains__('win32'):
+            wget.download(LINKPDFCONVERTER, PATHWIN32 + '\\pdfConverter.py')
+            wget.download(LINKDATACOLLECT, PATHWIN32 + '\\dataCollect.py')
+            wget.download(LINKEXPORTDATA, PATHWIN32 + '\\exportData.py')
+        else:
+            wget.download(LINKPDFTOTEXTMACOS, PATHMACOS + '/lib/pdftotxt')
+            wget.download(LINKPDFCONVERTER, PATHMACOS + '/pdfConverter.py')
+            wget.download(LINKDATACOLLECT, PATHMACOS + '/dataCollect.py')
+            wget.download(LINKEXPORTDATA, PATHMACOS + '/exportData.py')
 
+    def checkPyLib(self):
+        """
+            Controlla che tutte le librerie necessarie siano installate correttamente
+        """
+
+        com_d = os.popen('python3 -m pip list --disable-pip-version-check')
+        listd = com_d.read()
+
+        for i in range(len(LIBRARIES)):
+            if LIBRARIES[i] in listd:
+                pass
+            else:
+                os.system('python3 -m pip install ' + LIBRARIES[i] +
+                    ' -q --disable-pip-version-check')
+
+    def checkFirstInstallationMac(self):
+
+        dirPCDA = False
+        dirLib = False 
+        dirOut = False
+        dirPdf = False
+        dirTxt = False
+        checkDir = False
+        checkExe = False
+        checkpdfconverter = False
+        checkdatacollect = False
+        checkexportdata = False
+
+        files = os.listdir(PATHDOCUMENTSMAC)
+
+        for file in files:
+            if file == 'PdfConverter-DataAnalyzerTEST':
+                dirPCDA = True
+                break
+
+        if dirPCDA:
+            files = os.listdir(PATHMACOS)
+            
+            for file in files:
+                if file == 'lib':
+                    dirLib = True
+                if file == 'pdf':
+                    dirPdf = True
+                if file == 'txt':
+                    dirTxt = True
+                if file == 'output':
+                    dirOut = True
+            
+            if dirLib is True and dirPdf is True and dirTxt is True and dirOut is True:
+                checkDir = True
+            else:
+                if not dirLib:
+                    os.mkdir(PATHMACOS + '/lib')
+                if not dirPdf:
+                    os.mkdir(PATHMACOS + '/pdf')
+                if not dirTxt:
+                    os.mkdir(PATHMACOS + '/txt')
+                if not dirOut:
+                    os.mkdir(PATHMACOS + '/output')
+
+            if checkDir:
+                fileExe = os.listdir(PATHMACOS + '/lib')
+                for file in fileExe:
+                    if file == 'pdftotxt':
+                        checkExe = True
+                        break
+            
+            if not checkExe:
+                wget.download(LINKPDFTOTEXTMACOS, PATHMACOS + '/lib/pdftotxt')
+            
+            for file in files:
+                if file == 'pdfConverter.py':
+                    checkpdfconverter = True
+                if file == 'dataCollect.py':
+                    checkdatacollect = True
+                if file == 'exportData.py':
+                    checkexportdata = True
+
+            if checkpdfconverter is True and checkdatacollect is True and checkexportdata is True:
+                pass
+            else:
+                if not checkpdfconverter:
+                    wget.download(LINKPDFCONVERTER, PATHMACOS + '/pdfConverter.py')
+                if not checkdatacollect:
+                    wget.download(LINKDATACOLLECT, PATHMACOS + '/dataCollect.py')
+                if not checkexportdata:
+                    wget.download(LINKEXPORTDATA, PATHMACOS + '/exportData.py')
+
+        else:
+            os.mkdir(PATHMACOS)
+            for p in PATHSMACOS:
+                os.mkdir(PATHMACOS + p)
+            downloadUtilities()
+            os.system('chmod +x ' + PATHMACOS + '/lib/pdftotext')
 
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
+    ui.main()
     sys.exit(app.exec_())
